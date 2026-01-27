85b8590ccc96dc9c6fbb8cf0ae1649e3229cc790
Ytshak_Raiyner <yr3275744@gmail.com>
2026-01-27 09:44:40 +0200

    The first version of Satellite

---
+++ satellites.py
@@ -0,0 +1,8 @@
+import space_network_lib
+
+class Satellite(space_network_lib.SpaceEntity):
+    def __init__(self, name, distance_from_earth):
+        super().__init__(name, distance_from_earth)
+
+    def receive_signal(self, packet: space_network_lib.Packet):
+        print(f"{self.name} Received: {packet}.")
