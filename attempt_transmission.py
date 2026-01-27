eabc75f413ca74661cb78d69f889cfc8770d8ad4
Ytshak_Raiyner <yr3275744@gmail.com>
2026-01-27 16:23:43 +0200

    Handling of non-correctable disturbances, in the disturbance function and in the main

--- attempt_transmission.py
+++ attempt_transmission.py
@@ -1,6 +1,8 @@
 import satellites
 import time
 import space_network_lib
+class BrokenConnectionError(space_network_lib.CommsError):
+    pass
 def attempt_transmission(packet:space_network_lib.Packet, network:space_network_lib.SpaceNetwork):
     flag = 0
     while flag == 0:
@@ -12,3 +14,9 @@ def attempt_transmission(packet:space_network_lib.Packet, network:space_network_
             time.sleep(2)
         except space_network_lib.DataCorruptedError:
             print("Data corrupted, retrying...")
+        except space_network_lib.LinkTerminatedError:
+            print("lost Link")
+            raise BrokenConnectionError
+        except space_network_lib.OutOfRangeError:
+            print("Target out of range" )
+            raise BrokenConnectionError
